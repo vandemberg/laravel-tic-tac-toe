2251b0882724fc3b26ab95161bc3802acbb7c015
Vandemberg <vandemberg.silva.lima@gmail.com>
2018-12-19 23:24:28 -0300

    add new tests to verify the tic tac toe service layer better

--- tests/Services/TicTacToe/CheckWinnerTest.php
+++ tests/Services/TicTacToe/CheckWinnerTest.php
@@ -1,8 +1,56 @@
 <?php
 namespace Tests\Services\TicTacToe;
 
+use App\Models\Math;
+use App\Models\Move;
+use App\Services\TicTacToe\CheckWinner;
+use Tests\TestCase;
 
-class CheckWinnerTest
+class CheckWinnerTest extends TestCase
 {
+    /**
+     * Test to pass by the code and not change to win
+     */
+    public function testToNotWin()
+    {
 
+        $match = $this->createMatch();
+        foreach([0, 1, 2, 3, 4, 5, 6, 7, 8] as $position) {
+            $this->createMove($position, $match->id);
+        }
+
+        $winner = new CheckWinner();
+        $result = $winner->check($match, 0, 1);
+        $this->assertNotTrue($result);
+
+    }
+
+    /**
+     * Test to pass by the code and change to winner.
+     */
+    public function testToWin()
+    {
+        $match = $this->createMatch();
+
+        foreach([0, 1, 2, 3, 4, 5, 6, 7, 8] as $position) {
+            $this->createMove($position, $match->id);
+        }
+
+        foreach([0, 1, 2] as $position) {
+
+            $move = Move::where('position', $position)
+                ->where("match_id", $match->id)
+                ->first();
+
+            $move->value = 1;
+            $move->save();
+
+        }
+
+        $winner = new CheckWinner();
+        $result = $winner->check($match, $position, 1);
+
+        $this->assertTrue($result);
+
+    }
 }
--- tests/Services/TicTacToe/NextMoveTest.php
+++ tests/Services/TicTacToe/NextMoveTest.php
@@ -1,8 +1,36 @@
 <?php
 namespace Tests\Services\TicTacToe;
 
+use App\Services\TicTacToe\NextMove;
+use Tests\TestCase;
 
-class NextMoveTest
+class NextMoveTest extends TestCase
 {
+
+    public function testNextMoveAreX()
+    {
+
+        $matchInit = $this->createMatch();
+        $matchInit->next = 1;
+        $matchInit->save();
+
+        $expectedMove = 2;
+
+        $next = new NextMove();
+        $match = $next->change($matchInit);
+        $this->assertEquals($expectedMove, $match->next);
+
+    }
+
+    public function testNextMoveAreO()
+    {
+        $matchInit = $this->createMatch();
+        $expectedMove = 1;
+
+        $next = new NextMove($matchInit);
+        $match = $next->change($matchInit);
+        $this->assertEquals($expectedMove, $match->next);
+
+    }
 
 }
